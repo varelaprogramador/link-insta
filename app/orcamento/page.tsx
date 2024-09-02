caace4429b8fd3b2f61242896a91322baf1958e2
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2024-09-02 16:49:10 -0300

    commit intial

---
+++ app/orcamento/page.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import fundoComp from "../../public/assets/Fundo-Link.png";
+import fundoCard1 from "./assets/Image-Criar.png";
+import fundoCard2 from "./assets/Image-Refinar.png";
+
+import Link from "next/link";
+
+export default function Orcamento() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center p-24 justify-between gap-14 lg:max-h-screen overflow-hidden text-white`}
+      style={{
+        backgroundImage: `url(${fundoComp.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+
+      <div className="w-full flex justify-between items-start">
+        <Link href={"/"}>
+          <button className="p-2 bg-white text-black rounded-md font-semibold ">
+            &larr; Voltar</button>
+        </Link>
+
+        <Image width={400} height={400} src="/logo.svg" alt="logo-ryro" />
+        <div></div>
+      </div>
+      <section className="w-full grid grid-cols-2 gap-20 max-h-[400px] max-lg:grid-cols-1 max-lg:min-h-[400px]">
+        <Link href={"https://w.app/4Ti1rq"} className={`w-full h-[295px] rounded-md animate-slideInLeft`}>
+          <div
+            className={`w-full h-full rounded-md flex flex-col justify-end`}
+            style={{
+              backgroundImage: `url(${fundoCard1.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="w-full h-[40%] bg-black bg-opacity-35 p-4 flex flex-col gap-2">
+              <h2 className="font-semibold">Criar um site do zero</h2>
+              <p>Solicite um orçamento para ter seu próprio site criado do zero por Ryan Varela.</p>
+            </div>
+          </div>
+        </Link>
+
+        <Link href={"https://w.app/pqo7Yw"} className={`w-full h-[295px] rounded-md animate-slideInRight`}>
+          <div
+            className={`w-full h-full rounded-md flex flex-col justify-end`}
+            style={{
+              backgroundImage: `url(${fundoCard2.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="w-full h-[40%] bg-black bg-opacity-35 p-4 flex flex-col gap-2">
+              <h2 className="font-semibold">Refinar  meu  site </h2>
+              <p>Solicite um orçamento para ter seu  site remodelado por Ryan Varela.</p>
+            </div>
+          </div>
+        </Link>
+
+      </section>
+
+
+      <section className="max-w-[400px] min-h-36 flex justify-center items-end">
+        <h2>Criado por <a href="https://www.instagram.com/varela.dev/" className="underline">@Ryan Varela</a></h2>
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,60 @@
 import Image from "next/image";
+import fundoComp from "../public/assets/Fundo-Link.png";
+import fundoCard1 from "./assets/Image-Atendimento.png";
+import fundoCard2 from "./assets/Image-Portifolio.svg";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    <main
+      className={`flex min-h-screen flex-col items-center p-24 justify-between gap-14 lg:max-h-screen overflow-hidden text-white max-sm:p-4`}
+      style={{
+        backgroundImage: `url(${fundoComp.src})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          width={400}
+          height={400}
+          src="/logo.svg"
+          alt="logo-ryro"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <section className="w-full grid grid-cols-2 gap-20 max-h-[400px] max-lg:grid-cols-1 max-lg:min-h-[400px]">
+        <Link href={"/orcamento"} className={`w-full h-[295px] rounded-md animate-slideInLeft`}>
+          <div
+            className={`w-full h-full rounded-md flex flex-col justify-end`}
+            style={{
+              backgroundImage: `url(${fundoCard1.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="w-full h-[40%] bg-black bg-opacity-35 p-4 flex flex-col gap-2">
+              <h2 className="font-bold">Realizar Orçamento</h2>
+              <p>Solicite um orçamento para ter meus serviços.</p>
+            </div>
+          </div>
+        </Link>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        <Link href={""} className={`w-full h-[295px] cursor-not-allowed animate-slideInRight`}>
+          <div
+            className={`w-full h-full rounded-md`}
+            style={{
+              backgroundImage: `url(${fundoCard2.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          ></div>
+        </Link>
+      </section>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <section className="max-w-[400px] min-h-36 flex justify-center items-end">
+        <h2>Criado por <a href="https://www.instagram.com/varela.dev/" className="underline">@Ryan Varela</a></h2>
+      </section>
     </main>
   );
 }
